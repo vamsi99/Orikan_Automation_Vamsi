29ca025f0a5f7d304c8de9d54e243aff420b258a
jk <jk@DESKTOP-2DB9JIK>
2024-05-19 08:30:42 +0530

    Find duplicate words in java simplified way

--- src/main/java/com/orikan/test/DuplicateWords.java
+++ src/main/java/com/orikan/test/DuplicateWords.java
@@ -3,30 +3,20 @@ package com.orikan.test;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.regex.Pattern;
 
 public class DuplicateWords {
 
-	public static void main(String[] args) {
-		String sentence = "Java programming platform language is and Java is also a platform Java platform java";
-
-		System.out.println("Max and Min Duplicate words in the string are:");
-		findDuplicateWords(sentence);
-	}
-
 	public static void findDuplicateWords(String str) {
-		String[] words = str.split(" ");
+		String[] words = str.toLowerCase().split(" ");
 		Map<String, Integer> wordCountMap = new HashMap<>();
 
 		for (String word : words) {
-			String wordLowerCase = word.toLowerCase();
-			wordCountMap.put(wordLowerCase, wordCountMap.getOrDefault(wordLowerCase, 0) + 1);
+			wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
 		}
 		int maxValueInMap = Collections.max(wordCountMap.values()); // This will return max value in the HashMap
 
-		int minValueInMap = Collections.min(wordCountMap.values());
-
+		// int minValueInMap = Collections.min(wordCountMap.values());
+		System.out.println("Max and Min Duplicate words in the string are:");
 		for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
 			if (entry.getValue() == maxValueInMap) {
 				System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
@@ -34,15 +24,21 @@ public class DuplicateWords {
 				System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
 			}
 		}
-		
-		System.out.println("Duplicate words in the string are:"); 
-		
+
+		System.out.println("Duplicate words in the string are:");
+
 		for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
 			if (entry.getValue() > 1) {
 				System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
 			}
 		}
 
+	}
+
+	public static void main(String[] args) {
+		String sentence = "Java programming platform language platform is and Java platform is also a Java platform java";
+
+		findDuplicateWords(sentence);
 	}
 
 	/*
